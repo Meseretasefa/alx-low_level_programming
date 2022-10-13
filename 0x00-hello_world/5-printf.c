451dc2dbdff931d6abb0c0681d54d30fd3b65f8b
meseret asefa <you@example.com>
2022-10-13 11:26:49 -0700

    third commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 /*
-* File: 5-printf.c
-* Auth: Brennan D Baraban
-*/
+ * File: 5-printf.c
+ * Auth: Brennan D Baraban
+ */
 #include <stdio.h>
 
 /**
-*   main - Prints the size of various types.  
-*   Return: Always 0.
-*/
+ *   main - Prints the size of various types.  
+ *   Return: Always 0.
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
