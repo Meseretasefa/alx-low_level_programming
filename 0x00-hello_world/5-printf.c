88d598e81ee8ade84f1f9bc484a746fd90a3ce46
meseret asefa <you@example.com>
2022-10-13 11:07:23 -0700

    third commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,18 @@
 /*
- *  * File: 5-printf.c
- *   * Auth: Brennan D Baraban
- *    */
+ * File: 5-printf.c
+ * Auth: Brennan D Baraban
+ */
 #include <stdio.h>
+
 /**
- *  * main - Prints the size of various types.
+ *   main - Prints the size of various types.
  * 
  *    
- *     * Return: Always 0.
- *      */
+ *      Return: Always 0.
+ */
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-			return (0);
+	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,6 +4,7 @@
  */
 
 #include <stdio.h>
+
 /**
  *   main - Prints the size of various types based on
  *           the computer it is compiled and run on..
