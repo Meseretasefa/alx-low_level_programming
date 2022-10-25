f63bef747381cdeb22d47b0d7aafd67dee5ea417
meseret asefa <you@example.com>
2022-10-25 02:01:55 -0700

    second chance

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,27 +1,29 @@
 /*
- *  * File: 9-strcpy.c
- *   * Auth: meseret asefa
- *    */
+ * File: 9-strcpy.c
+ * Auth: meseret asefa
+ */
 
 #include "meseret.h"
 
 /**
- *  * _strcpy - Copies a string pointed to by @src, including the
- *   *           terminating null byte, to a buffer pointed to by @dest.
- *    * @dest: A buffer to copy the string to.
- *     * @src: The source string to copy.
- *      *
- *       * Return: A pointer to the destination string @dest.
- *        */
-char *_strcpy(char *dest, const char *src)
+ * _strcpy - Copies a string pointed to by src, including the
+ *           terminating null byte, to a buffer pointed to by dest.
+ * @dest: The buffer storing the string copy.
+ * @src: The source string.
+ *
+ * Return: The pointer to dest.
+ */
+char *_strcpy(char *dest, char *src)
 {
-		int index = 0;
+	int index = 0, len = 0;
 
-			while (src[index])
-					{
-								dest[index] = src[index];
-										index++;
-											}
+	while (src[index++])
+		len++;
 
-				return (dest);
+	for (index = 0; index < len; index++)
+		dest[index] = src[index];
+
+	dest[index] = '\0';
+
+	return (dest);
 }
