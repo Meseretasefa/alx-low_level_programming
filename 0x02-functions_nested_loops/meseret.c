6b5a6c0b5b722f3127f8d17712e90f659afb6002
meseret asefa <you@example.com>
2022-10-25 05:26:30 -0700

    nested loop

---
+++ 0x02-functions_nested_loops/meseret.c
@@ -0,0 +1,26 @@
+/*
+ * File: meseret.c
+ * Auth: meseret asefa
+ *
+ */
+
+#include "meseret.h"
+
+/**
+ * main - Prints "meseret" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
