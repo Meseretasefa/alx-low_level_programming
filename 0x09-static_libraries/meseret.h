8d3755c780d41d4eb500ffe4ab8d9b4714995fbf
meseret asefa <you@example.com>
2022-11-04 06:22:12 -0700

    static library

--- 0x09-static_libraries/meseret.h
+++ 0x09-static_libraries/meseret.h
@@ -1,5 +1,13 @@
-#ifndefine meseret_h
-#define meseret_h
+#ifndef MESERET_H
+#define MESERET_H
+
+/*
+ * File: holberton.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file containing the prototypes of all functions
+ *       included in the static library libholberton.a.
+ */
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -20,4 +28,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
 #endif
